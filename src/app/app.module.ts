1ad7d68f9af503fb2b713177957e4593675960ee
Prudvi97 <89538003+Prudvi97@users.noreply.github.com>
2023-02-14 14:34:37 -0600

    Routing changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {CommonModule} from '@angular/common';
@@ -8,6 +9,10 @@ import { CoursesComponent } from './courses/courses.component';
 import { CoursesComponentHtmlComponent } from './courses.component.html/courses.component.html.component';
 import { LikeComponent } from './like/like.component';
 import { ZippyComponent } from './zippy/zippy.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ArchiveComponent } from './archive/archive.component';
 
 @NgModule({
   declarations: [
@@ -15,13 +20,28 @@ import { ZippyComponent } from './zippy/zippy.component';
     CoursesComponent,
     CoursesComponentHtmlComponent,
     LikeComponent,
-    ZippyComponent
+    ZippyComponent,
+    ContactFormComponent,
+    HomeComponent,
+    NotFoundComponent,
+    ArchiveComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'home', 
+      component: HomeComponent 
+    },
+      { path: 'archive/:year/:month', 
+      component: ArchiveComponent 
+    },
+      { path: '**', 
+      component: NotFoundComponent
+    },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+archives = [
+  { year: 2017, month: 1 },
+  { year: 2018, month: 2 },
+  { year: 2019, month: 3 },
+];
+}
